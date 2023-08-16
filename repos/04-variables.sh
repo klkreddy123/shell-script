46887a079ce905e2a91bff77f5bfbc68c7597151
klkreddy123 <lakshmikanthr.kudumula@gmail.com>
2023-08-16 13:35:56 +0530

    added hello world

--- repos/04-variables.sh
+++ repos/04-variables.sh
@@ -5,6 +5,14 @@
 NUMBER1=1
 NUMBER2=2
 
-SUM=$(NUMBER1+NUMBER2)
+SUM=$((NUMBER1+NUMBER2))
+
+echo "sum of 2 numbers is $SUM"
+
+
+NUMBER3=$1
+NUMBER4=$2
+
+SUM=$((NUMBER3+NUMBER4))
 
 echo "sum of 2 numbers is $SUM"
