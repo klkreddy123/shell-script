a6e86a7a053593f2a4cfc7a4fbcf857c6ad45ea6
klkreddy123 <lakshmikanthr.kudumula@gmail.com>
2023-08-16 12:42:32 +0530

    added hello world

--- repos/01-hello-world.sh
+++ repos/01-hello-world.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "Hello world"
+
+echo "Suresh: Hi Ramesh how are you?"
+echo "Ramesh: Hi Suresh I am Fine"
+echo "Ramesh: Hi Suresh how are you?"
+echo "Suresh: Hi Ramesh I am Fine"
