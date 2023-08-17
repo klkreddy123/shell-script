93579fb5ebcd9abbdf15f2f73270c012cc61a99c
klkreddy123 <lakshmikanthr.kudumula@gmail.com>
2023-08-17 11:39:33 +0530

    added conditions

--- repos/07-conditions.sh
+++ repos/07-conditions.sh
@@ -6,8 +6,8 @@ NUMBER=$1
 
 if [ $NUMBER -gt 10 ]
 then 
-  echo"$NUMBER is greater than 10"
+  echo "$NUMBER is greater than 10"
 else
-   echo"$NUMBER is lesser than 10"
+   echo "$NUMBER is lesser than 10"
 fi
 
