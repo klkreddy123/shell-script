438b7b6adf82f473a061aacb7836d9d82096dc87
klkreddy123 <lakshmikanthr.kudumula@gmail.com>
2023-08-17 18:23:18 +0530

    added conditions

--- repos/09-functions.sh
+++ repos/09-functions.sh
@@ -8,3 +8,12 @@ then
 else
     echo "user is root user"
 fi
+yum install mysql -y
+VALIDATE(){
+#$1 will receive the first argument
+if [ $? -ne 0 ]
+then
+    echo "Installation is failure"
+else
+    echo "Installation is sucess"
+}
