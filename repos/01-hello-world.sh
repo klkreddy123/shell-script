d6a123456793374e43e09803bd46decfa6fbacf6
klkreddy123 <lakshmikanthr.kudumula@gmail.com>
2023-08-18 10:13:16 +0530

    added hello world

--- repos/01-hello-world.sh
+++ repos/01-hello-world.sh
@@ -5,7 +5,7 @@ echo "***************************************"
 
 PERSON1=Suresh
 PERSON2=Rakesh
-echo " $(PERSON1): How are you $(PERSON2)?"
-echo " $(PERSON2): How are you $(PERSON1)?"
-echo " $(PERSON1): I am fine $(PERSON2)"
-echo " $(PERSON2): I am fine $(PERSON1)"
+echo " $PERSON1: How are you $PERSON2?"
+echo " $PERSON2: How are you $PERSON1?"
+echo " $PERSON1: I am fine $PERSON2"
+echo " $PERSON2: I am fine $PERSON1"
