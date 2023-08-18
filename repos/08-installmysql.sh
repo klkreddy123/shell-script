52cdef6eed285b9ea719ed879b813ea68f998578
klkreddy123 <lakshmikanthr.kudumula@gmail.com>
2023-08-18 12:40:59 +0530

    08-installations

--- repos/08-installmysql.sh
+++ repos/08-installmysql.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
 # our program goal is to install my sql
-
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
+if [$USERID -ne 0 ]
 then
-    echo "ERROR:: Please run this script with root access"
+    echo "logged in user is not root user"
 else
-    echo "INFO:: logged in user is root user"
-fi
-
+    echo "logged in user is root user"
+fi    
 yum install mysql -y
-
-if [ $? -ne 0 ]
-then
-    echo "Installation of my sql is not sucess"
-    exit 1
-else
-    echo "Installation of my sql is sucess"
-fi
-
