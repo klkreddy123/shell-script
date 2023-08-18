e23e6336b8b284347bbd58f53fd3677d8d7f4ab3
klkreddy123 <lakshmikanthr.kudumula@gmail.com>
2023-08-18 10:15:51 +0530

    varibales

--- repos/02-variables.sh
+++ repos/02-variables.sh
@@ -2,4 +2,5 @@
 
 #shell script run the command inside $() and stores the output into the varibale called DATE.
 DATE=$(date)
-echo "This is exected at timestamp $DATE"
+
+echo "Todays date is: DATE"
