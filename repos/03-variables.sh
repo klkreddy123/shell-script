eb2c1b884a3d993895166539774f7dc5f625d411
klkreddy123 <lakshmikanthr.kudumula@gmail.com>
2023-08-18 10:19:53 +0530

    varibales

--- repos/03-variables.sh
+++ repos/03-variables.sh
@@ -1,7 +1,5 @@
 PERSON1=$1 # this is the first value from the command line
 PERSON2=$2 # this is the second value from the command line
 
-echo "$PERSON1: Hi $PERSON2 how are you?"
-echo "$PERSON2: Hi $PERSON1 I am Fine"
-echo "$PERSON2: Hi $PERSON1 how are you?"
-echo "$PERSON1: Hi $PERSON2 I am Fine"
+echo " $PERSON1: How are you? "
+echo " $PERSON2: I am fine. "
