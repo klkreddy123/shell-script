7a4587ee86bfb534396b0b4404f8f10341778068
klkreddy123 <lakshmikanthr.kudumula@gmail.com>
2023-08-19 21:19:01 +0530

    12-deleteoldfiles

--- repos/12-deleteoldfiles.sh
+++ repos/12-deleteoldfiles.sh
@@ -3,7 +3,8 @@
 APP_LOGS_DIR=/home/centos/shell-script/repos/app-logs
 DATE=$(date +%F)
 SCRIPT_NAME=$0
-LOGFILE=$APP_LOGS_DIR/$SCRIPT_NAME-$DATE.log
+LOGSDIR=/home/centos/shellscript-logs
+LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
 
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 
