a5b88e32bc42d4c92a09708c74c213f48e9413c2
klkreddy123 <lakshmikanthr.kudumula@gmail.com>
2023-08-19 18:58:34 +0530

    09-functions

--- repos/09-functions.sh
+++ repos/09-functions.sh
@@ -3,6 +3,17 @@
 
 USERID=$(id -u)
 
+VALIDATE(){
+
+if [ $1 -ne 0 ]
+    echo "$2 is failure"
+    exit 1
+else
+    echo "$2 is success"
+fi
+
+}
+
 if [ $USERID -ne 0 ]
 then
     echo "Failure: Logged in user is not root user"
@@ -13,6 +24,8 @@ fi
 
 sudo yum install mysql -y
 
+VALIDATE $? "Installing mysql"
+
 if [ $? -ne 0 ]
 then
     echo "Installtion of my sql is failure"
@@ -21,13 +34,6 @@ else
     echo "Installation of my sql is success"
 fi
 
-sudo yum install postfixe -y
+sudo yum install postfix -y
 
-if [ $? -ne 0 ]
-then
-    echo "Installtion of my postfix is failure"
-    exit 1
-else
-    echo "Installation of my postfix is success"
-fi
-
+VALIDATE $? "Installing postfix"
