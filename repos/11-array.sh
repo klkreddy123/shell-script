cc86cdef7a1458c930534a5c2a1f5792514ea874
klkreddy123 <lakshmikanthr.kudumula@gmail.com>
2023-08-20 16:58:29 +0530

    11-array

--- repos/11-array.sh
+++ repos/11-array.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
+# implement log files
+# implement colors
+# implement validations
+# implement validations through functions
+# your script should check package is already installed or not, if already installed print yellow color
+# if installed just print package is alredy installed, it should not run install command
+
+if [ $? -ne 0 ]
+then
+    echo "Fail... Logged in user is not root user"
+    exit 1
+else
+    echo " Success-- Logged in user is root user"
+fi
 
 for i in $@
 do
-    sudo yum install $i
+    sudo yum install $i -y
     echo" Installing $i"
 done
