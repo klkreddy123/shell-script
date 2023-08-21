253330dc1c0dc172bd99a54d51cd7fa955e08c69
klkreddy123 <lakshmikanthr.kudumula@gmail.com>
2023-08-21 20:56:52 +0530

    13-diskusage

--- repos/13-diskusage.sh
+++ repos/13-diskusage.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 
-LOG_FILE_DIRECTORY=/tmp
-DATE=$(date +%F+%H:%M:%S)
+# colors
+# validations
+# log redirections
+
+LOGFILE_DIRECTORY=/tmp
+DATE=$(date +%F:%H:%M:%S)
 SCRIPT_NAME=$0
-LOG_FILE=$LOG_FILE_DIRECTORY/SCRIPT_NAME=$0-$DATE.log
+LOGFILE=$LOGFILE_DIRECTORY/$SCRIPT_NAME-$DATE.log
 
 R="\e[31m"
 G="\e[32m"
-NC="\e[0m"
+N="\e[0m"
 Y="\e[33m"
 
-DISH_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
-DISK_USAGE_THRESHORLD=1
+DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
+DISK_USAGE_THRESHOLD=1
 message=""
 
-#IFS means internal field separator
+#IFS= means internal feild seperator is space.
 while IFS= read line
-do
-    #echo "output: $line"
-    #this command will give usage in no format for comparision
+do  
+    # this command will give you usage in number format for comparision
     usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
     # this command will give us partition
     partition=$(echo $line | awk '{print $1}')
-    #now we need to check whether it is more than threshold or not
-    if [ $usage -gt $DISK_USAGE_THRESHORLD ];
+    #now you need to check whether it is more than threshold or not
+    if [ $usage -gt $DISK_USAGE_THRESHOLD ];
     then
-        message+="HIGH DISK USAGE ON $partition: $usage"
+        message+="HIGH DISK USAGE on $partition: $usage\n"
     fi
 done <<< $DISK_USAGE
 
-echo "message: $message"
+echo -e "message: $message"
 
+#echo "$message" | mail -s "High Disk usage" info@joindevops.com
 
+#how to call other shell script from your current script
+sh mail.sh klkreddy2268@gmail.com "High Disk Usage" "$message" "QA TEAM" "High Disk usage"
