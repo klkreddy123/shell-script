640e748cc31fd2bf46b319f19806eb52d955b85f
klkreddy123 <lakshmikanthr.kudumula@gmail.com>
2023-08-21 21:22:44 +0530

    13-diskusage

--- repos/mail.sh
+++ repos/mail.sh
@@ -8,4 +8,8 @@ BODY=$3
 TEAM_NAME=$4
 ALERT_TYPE=$5
 
-echo "all args: $@"
+#echo "all args: $@"
+
+FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/g" template.html)
+
+echo "$BODY" | mail -s "$SUBJECT" "$TO
